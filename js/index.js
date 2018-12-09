83cde528699dfe836b2637fdb4f43a125b239311
SeongEunPark <Sungeun@Userui-MacBook-Pro.local>
2018-12-10 03:31:45 +0900

    #1 init commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const EXAMPLE_DATA = [
+  {
+    name : "data1",
+    value : 50
+  },
+  {
+    name : "data2",
+    value : 100
+  },
+  {
+    name : "data3",
+    value : 70
+  }
+]
+
+const width = 500;
+const height = 500;
+
+const widthScale = d3.scaleLinear()
+  .domain([0,100])
+  .range([0, width]);
+
+const colorLinear = d3.scaleLinear()
+  .domain([0, 100])
+  .range(["blue", "red"]);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let canvas = d3.select("body").append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+    .attr("transform", "translate(50, 50)")
+    .style("border", "black solid 1px");
+
+  let bars = canvas.selectAll("rect")
+    .data(EXAMPLE_DATA)
+    .enter()
+      .append("rect")
+      .attr("width", function (d) { return widthScale(d.value); })
+      .attr("height", function (d, i) { return 50; })
+      .attr("fill", function (d) { return colorLinear(d.value) })
+      .attr("y", function (d, i) { return i * 100 })
+
+})
