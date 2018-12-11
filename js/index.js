2f2204d857e2bde7a48e61071542c00134186aa7
SungeunP <sungeun1214@gmail.com>
2018-12-11 22:12:53 +0900

    add desc

--- js/index.js
+++ js/index.js
@@ -16,21 +16,25 @@ const EXAMPLE_DATA = [
 const width = 500;
 const height = 500;
 
+// scaleLinear
 const widthScale = d3.scaleLinear()
   .domain([0,100])
   .range([0, width]);
 
+// color scaleLinear
 const colorLinear = d3.scaleLinear()
   .domain([0, 100])
   .range(["blue", "red"]);
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Add svg
   let canvas = d3.select("body").append("svg")
     .attr("width", 500)
     .attr("height", 500)
     .attr("transform", "translate(50, 50)")
     .style("border", "black solid 1px");
 
+  // Add bars
   let bars = canvas.selectAll("rect")
     .data(EXAMPLE_DATA)
     .enter()
@@ -38,6 +42,6 @@ document.addEventListener("DOMContentLoaded", function () {
       .attr("width", function (d) { return widthScale(d.value); })
       .attr("height", function (d, i) { return 50; })
       .attr("fill", function (d) { return colorLinear(d.value) })
-      .attr("y", function (d, i) { return i * 100 })
-
+      .attr("y", function (d, i) { return i * 100 });
+  
 })
